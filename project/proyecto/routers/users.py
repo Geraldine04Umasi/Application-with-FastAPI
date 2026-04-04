c347626ac2a239626cc0875f7b13bf0aa6bc61fa
Geraldine Umasi <gumasi@unsa.edu.pe>
2026-04-03 19:10:39 -0500

    Add login endpoint for user authentication

--- project/proyecto/routers/users.py
+++ project/proyecto/routers/users.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException, APIRouter
+from fastapi.security import HTTPBasicCredentials
 from ..database import User
 from ..schemas import UserRequestModel, UserResponseModel
 
@@ -16,4 +17,16 @@ async def create_user(user: UserRequestModel):
         password=hash_password
     )
 
+    return user
+
+@router.post('/login', response_model=UserResponseModel)
+async def login(credentials: HTTPBasicCredentials):
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
     return user
